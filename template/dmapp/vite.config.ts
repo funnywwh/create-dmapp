81e20553360b65d1a86288be4d0da9f4795602d3
winger <wuwenhui@rightchaintech.com>
2023-10-30 09:14:33 +0800

    1.更新模板

--- template/dmapp/vite.config.ts
+++ template/dmapp/vite.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig } from 'vite'
 import packageJson from './package.json'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:`/dmapp/${packageJson.dmappId}`,
-  server:{
-    host:"0.0.0.0",
-  },
-  plugins: [vue()],
+export default defineConfig(async (env: ConfigEnv) => {
+  let base = `/dmapp/${packageJson.dmappId}`
+  let port = 9090
+  if (env.mode == 'development') {
+    base += `?debug=true&port=${port}`
+  }
+  console.log(env,base)
+  return {
+    base: base,
+    clearScreen:false,
+    server: {
+      host: "0.0.0.0",
+      port: port
+    },
+    plugins: [vue()],
+  }
 })
